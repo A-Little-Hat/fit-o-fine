cd1e4a4dd8b5ca1c75827949a0320d5aff47dd1b
a-little-hat <soumyasarkar309@gmail.com>
2024-03-14 01:22:41 +0530

    profile UI update

--- client/src/Routes/User/Profile.js
+++ client/src/Routes/User/Profile.js
@@ -1,76 +1,94 @@
 import React from "react";
 import { connect } from "react-redux";
 import Navbar from "../../Components/Navbar";
-import { Card } from "react-bootstrap";
+// import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 function Profile({ user }) {
     return (
         <>
             <Navbar />
-            
+
             <div className="container my-4" >
                 {/* TODO: Later here display metamask details */}
-                <Card className="mb-3" style={{backgroundColor:'#F1F3F3'}}>
-                    <Card.Body>
-                        <p className="text-center text-muted m-0">
-                           <font color="black"> You are login with metamask</font>
-                        </p>
-                    </Card.Body>
+                {/* <Card className="mb-3" style={{backgroundColor:'#F1F3F3'}}>
+                    <Card.Body> */}
+                <p className="text-center text-muted m-0">
+                    <font color="black"> You are login with metamask</font>
+                </p>
+                {/* </Card.Body>
                 </Card>
                 <Card style={{backgroundColor:'#F1F3F3'}}>
-                    <Card.Body>
-                        {user ? (
-                            <>
-                                <div >
-                                    <span
-                                        className="material-icons-two-tone"
-                                        style={{ fontSize: "4rem" }}
-                                    >
-                                                                            </span>
-                                </div>
-                                <h5 className="card-title" >
-                                     {user.name}{" "}
+                    <Card.Body> */}
+                {user ? (
+                    // <>
+                    //     <h5 className="card-title" >
+                    //          {user.name}{" "}
+                    //         {user.role === "admin" && "(Admin)"}
+                    //         {user.role !== "admin" && (
+                    //             <>
+                    //                 {" "}
+                    //                 {user.verified
+                    //                     ? "(Verified)"
+                    //                     : "(Not Verified)"}
+                    //             </>
+                    //         )}
+                    //     </h5>
+                    //     <hr />
+                    //     <span className=" mb-0 text-muted">
+                    //         Address
+                    //     </span>
+                    //     <span>{user.addr}</span>
+                    //     <span className=" mb-0 text-muted">Email</span>
+                    //     <span>{user.email}</span>
+                    //     <span className=" mb-0 text-muted">
+                    //         Contact Number
+                    //     </span>
+                    //     <span>{user.con}</span>
+                    //     <span className=" mb-0 text-muted">Type
+                    //     </span>
+                    //     <span>{user.type}</span> 
+                    // </>
+                    <div class="container">
+                        <div class="card mt-5">
+                            <div class="card-header">
+                                <h5 class="card-title">
+                                    {user.name}
                                     {user.role === "admin" && "(Admin)"}
                                     {user.role !== "admin" && (
                                         <>
-                                            {" "}
-                                            {user.verified
-                                                ? "(Verified)"
-                                                : "(Not Verified)"}
+                                            {user.verified ? "(Verified)" : "(Not Verified)"}
                                         </>
                                     )}
                                 </h5>
-                                <hr />
-                                <p className=" mb-0 text-muted">
-                                    Address
-                                </p>
-                                <p>{user.addr}</p>
-                                <p className=" mb-0 text-muted">Email</p>
-                                <p>{user.email}</p>
-                                <p className=" mb-0 text-muted">
-                                    Contact Number
-                                </p>
-                                <p>{user.con}</p>
-                                <p className=" mb-0 text-muted">Type
-                                </p>
-                                <p>{user.type}</p>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                <br/>
-                                
-                            </>
-                        
-                        ) 
-                        : (
-                            <p>
-                                Please register first then you will use out
-                                dapp. <Link to="/register">Register</Link>
-                            </p>
-                        )}
-                    </Card.Body>
-                </Card>
+                            </div>
+                            <div class="card-body">
+                                <ul class="list-unstyled">
+                                    <li class="mb-2">
+                                        <span class="text-muted">Address:</span> {user.addr}
+                                    </li>
+                                    <li class="mb-2">
+                                        <span class="text-muted">Email:</span> {user.email}
+                                    </li>
+                                    <li class="mb-2">
+                                        <span class="text-muted">Contact Number:</span> {user.con}
+                                    </li>
+                                    <li class="mb-2">
+                                        <span class="text-muted">Type:</span> {user.type}
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                )
+                    : (
+                        <p>
+                            Please register first then you will use out
+                            dapp. <Link to="/register">Register</Link>
+                        </p>
+                    )}
+                {/* </Card.Body>
+                </Card> */}
             </div>
         </>
     );
